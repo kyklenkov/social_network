f08ad482730e5c0ad5443567eb5565fe2f1b379c
Mikhail Kuklenkov <kyklenkov@mail.ru>
2023-11-28 20:15:29 +0300

    lesson 67

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,20 +2,10 @@ import React from 'react';
 import Header from "./Header";
 import {connect} from "react-redux";
 import {getAuthUserData, setAuthUserData} from "../Redux/auth-reducer";
-import {authAPI} from "../../api/api";
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
         this.props.getAuthUserData();
-
-/*
-        authAPI.me()
-            .then(response => {
-                if (response.data.resultCode === 0) {
-                    let {id, login, email} = response.data.data;
-                    this.props.setAuthUserData(id, email, login);
-                }
-            });*/
     }
 
     render () {
