82ff3db15538a1cffd93e32850101bd1973935b3
Mikhail Kuklenkov <kyklenkov@mail.ru>
2024-03-01 19:24:23 +0300

    lesson 75

--- src/components/Redux/Redux-store.js
+++ src/components/Redux/Redux-store.js
@@ -5,13 +5,15 @@ import sidebarReducer from "./Sidebar-Reducer";
 import usersReducer from "./Users-Reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
